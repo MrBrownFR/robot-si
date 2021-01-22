759b5d843f1f116c9f8574b098d35008141d73fd
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-01-22 11:50:01 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
-maqueenIR.onPressEvent(RemoteButton.Power, function () {
-	
-})
-let distance = 0
-let speed = 255
+let speed = 0
 let time = 0
-while (distance < 100) {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
+let distance = 0
+basic.forever(function () {
+    speed = 255
     time = 0
-    distance = 4.3 * Math.PI * (speed * 85.5 / 255)
-}
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    while (distance < 100) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
+        time = 0
+        distance = 4.3 * Math.PI * (speed * 85.5 / 255)
+    }
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+})
