394815b9325d8a8ec607e30d8be43298ba566e47
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-01-22 11:56:15 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-let speed = 0
-let time = 0
-let distance = 0
 basic.forever(function () {
-    speed = 255
-    time = 0
-    while (distance < 100) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
-        time = 0
-        distance = 4.3 * Math.PI * (speed * 85.5 / 255)
-    }
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
 })
