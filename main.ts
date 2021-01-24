973287e4e3751793a4ccbc98d97919c77b364f04
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-01-24 14:12:21 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 let time = 0
 let speed = 255
+// Calcule la distance en fonction des cm/sec, avec un produit en croix sur la base du 85.5 tr/min
 while (time < 100 * 60 * 1155) {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
     time = control.millis() * 1000
