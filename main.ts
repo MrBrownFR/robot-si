d29564b2cd611cb90e1a3cfe28ea8841b8c83862
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-01-24 18:24:34 +0100

    Reprise de zéro à cause d'un bug

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
+let distance = 0
+let time = 0
+while (time < 100 * 60 / 1155) {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+}
+maqueen.motorStop(maqueen.Motors.M1)
+basic.forever(function () {
+    time = control.millis() * 1000
 })
