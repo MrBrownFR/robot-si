ccfbd884fab88f2d54cf74353cd0fcefd0b3a2fb
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-01-24 18:28:01 +0100

    Correction d'erreur

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ let time = 0
 while (time < 100 * 60 / 1155) {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
 }
-maqueen.motorStop(maqueen.Motors.M1)
+maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
     time = control.millis() * 1000
 })
