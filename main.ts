f190b019631778fae00bc8429d9918f04dcd4c10
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-02-05 11:59:52 +0100

    Ajout des bases du contrôle RC

--- main.ts
+++ main.ts
@@ -1,13 +1,64 @@
+let time = 0
+function right (num: number) {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motorStop(maqueen.Motors.M2)
+}
+function left (num: number) {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    maqueen.motorStop(maqueen.Motors.M1)
+}
+function backward () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 218)
+}
+maqueen.IR_callbackUser(function (message) {
+    if (message == 64) {
+        forward()
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (message == 25) {
+        backward()
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (message == 7) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (message == 9) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (message == 21) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showIcon(IconNames.No)
+    }
+})
 function forward () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 218)
 }
-let distance = 0
-let time = 0
-forward()
-basic.pause(Math.floor(100 * 60 / 1155 * 1000))
-maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
-    basic.showString("POUSSEZ-VOUS")
     time = control.millis() * 1000
 })
