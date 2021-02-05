a13d123c534f164ed66198ea27ec46f4514ec6a9
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-02-05 10:44:14 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
+function forward () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 218)
+}
 let distance = 0
 let time = 0
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-basic.pause(Math.round(100 * 60 / 1155 * 1000))
+forward()
+basic.pause(Math.floor(100 * 60 / 1155 * 1000))
 maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
-    basic.showString("STONKS")
+    basic.showString("POUSSEZ-VOUS")
     time = control.millis() * 1000
 })
