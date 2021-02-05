120a701da692be5067465fef3949fb8ee6be78cb
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-02-05 16:01:34 +0100

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,67 @@
-let sprite = "STONKS"
-let distance = 0
-let time = 0
-while (time < 100 * 60 / 1155) {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-}
-maqueen.motorStop(maqueen.Motors.All)
-basic.forever(function () {
-    time = control.millis() * 1000
-    sprite.scrollImage(1, 200)
-})
+def right(num: number):
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motor_stop(maqueen.Motors.M2)
+    basic.show_leds("""
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        """)
+def left(num: number):
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    maqueen.motor_stop(maqueen.Motors.M1)
+    basic.show_leds("""
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        """)
+def backward():
+    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CCW, 255)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 218)
+    basic.show_leds("""
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        """)
+def auto():
+    forward()
+    basic.pause(Math.floor(100 * 60 / 1155 * 1000))
+    maqueen.motor_stop(maqueen.Motors.ALL)
+
+def on_ir_callbackuser(message):
+    if message == 64:
+        forward()
+    if message == 25:
+        backward()
+    if message == 7:
+        left(1)
+    if message == 9:
+        right(1)
+    if message == 21:
+        maqueen.motor_stop(maqueen.Motors.ALL)
+        basic.show_icon(IconNames.NO)
+maqueen.IR_callbackUser(on_ir_callbackuser)
+
+def forward():
+    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 255)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 218)
+    basic.show_leds("""
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        """)
+time = 0
+basic.show_icon(IconNames.YES)
+
+def on_forever():
+    global time
+    basic.pause(1)
+    time += 1 / 1000
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,64 +1,76 @@
-let time = 0
 function right (num: number) {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
     maqueen.motorStop(maqueen.Motors.M2)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
 }
 function left (num: number) {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     maqueen.motorStop(maqueen.Motors.M1)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
 }
 function backward () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 218)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+}
+function auto () {
+    forward()
+    basic.pause(Math.floor(100 * 60 / 1155 * 1000))
+    maqueen.motorStop(maqueen.Motors.All)
 }
 maqueen.IR_callbackUser(function (message) {
     if (message == 64) {
         forward()
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
     }
     if (message == 25) {
         backward()
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
     }
     if (message == 7) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
+        left(1)
     }
     if (message == 9) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
+        right(1)
     }
     if (message == 21) {
         maqueen.motorStop(maqueen.Motors.All)
         basic.showIcon(IconNames.No)
     }
+    if (message == 67) {
+        auto()
+    }
 })
 function forward () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 218)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
 }
+let time = 0
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    time = control.millis() * 1000
+    basic.pause(1)
+    time += 1 / 1000
 })
