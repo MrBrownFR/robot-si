30cc38aeec4408a7b838fbfa6e0a921a76914bba
MrBrownFR <51024678+MrBrownFR@users.noreply.github.com>
2021-04-02 11:13:46 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -37,12 +37,10 @@ function auto () {
             forward()
         } else {
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
             }
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
             }
         }
     }
